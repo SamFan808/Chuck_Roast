ad0de5008b9dd4d32df58db6ca45f8d636b23d5f
Sam Fan <samisfan@msn.com>
2020-11-03 22:56:32 -0700

    added functionality to the search bar, and started working on the results page-jump-thing that keeps everything in index.html

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var mealsSeafood =
   "https://www.themealdb.com/api/json/v1/1/filter.php?c=seafood";
 var norrisFoodQuotes = "https://api.chucknorris.io/jokes/random?category=food";
 
+
+
 // $("#random").on("click", function () {
 //   fetch(randomMeal)
 //     .then(function (response) {
@@ -86,6 +88,47 @@ var norrisFoodQuotes = "https://api.chucknorris.io/jokes/random?category=food";
 //     console.log("random Norris food quote");
 //     console.log(data);
 //   });
+// fetch function for search input ===============
+
+$(".button").on("click", function () {
+  var mealInput = document.querySelector(".input-group-field");
+  var meal = mealInput.value;
+  var mealSearch = "https://www.themealdb.com/api/json/v1/1/search.php?s="+ meal;
+  fetch(mealSearch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (meal === "") {
+        console.log("bad input"); // let's put a modal alert here that says - "please input some text to search"
+      } else if (data.meals === null) {
+        console.log("try again"); // let's put another modal alert here that reads "Sorry, meal not found. Try again"
+      } else {
+        // anchor tag that takes user to a div tag with a specific ID, smooth scroll takes user to page 2, allows for return navigation by using "back"
+        renderCell();
+      }
+    });
+    mealInput.value = ""; // resets the search field
+});
+
+function renderCell () {
+  location.href ="index.html#results";
+  var grid = $("#gridTarget");
+  var cell = $("<section>");
+  var card = $("<section>");
+  var cardBody = $("<section>");
+
+  cell.addClass("cell");
+  card.addClass("card");
+  cardBody.addClass("card-body");
+  cardBody.attr("id", "result1");
+
+  grid.append(cell);
+  cell.append(card);
+  card.append(cardBody);
+  console.log("it's working");
+}
 
 $(".cell").on("click", function (event) {
   event.preventDefault();
@@ -111,3 +154,6 @@ $(".cell").on("click", function (event) {
 
   console.log(category);
 });
+
+
+// localStorage feature goes here ========================
---
+++ script2.js
@@ -0,0 +1,18 @@
+function renderCell () {
+  // var grid = $("#gridTarget");
+  // var cell = $("<div>");
+  // var card = $("<div>");
+  // var cardBody = $("<div>");
+
+  // cell.addClass("cell");
+  // card.addClass("card");
+  // cardBody.addClass("card-body");
+  // cardBody.id("result1");
+
+  // grid.append(cell);
+  // cell.append(card);
+  // card.append(cardBody);
+  console.log("it's working");
+}
+
+renderCell();
